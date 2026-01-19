229d9fb9c430a6cd1979151dc662c711f8cd86c4
wyjscg <1455573939@qq.com>
2026-01-19 15:50:52 +0800

    Update read.rs

--- read.rs
+++ read.rs
@@ -1,10 +1,68 @@
-// SecretNamespaceLister 帮助列出和获取 Secrets。
-// 这里返回的所有对象必须被视为只读。
-trait SecretNamespaceLister {
-    // List 列出给定命名空间的索引器中的所有 Secrets。
-    // 这里返回的对象必须被视为只读。
-    fn list(&self, selector: labels::Selector) -> Result<Vec<&corev1::Secret>, Box<dyn std::error::Error>>;
-    // Get 从给定命名空间和名称的索引器中检索 Secret。
-    // 这里返回的对象必须被视为只读。
-    fn get(&self, name: &str) -> Result<&corev1::Secret, Box<dyn std::error::Error>>;
+fn authenticate_token(&self, ctx: context::Context, token: String) -> Result<(Option<authenticator::Response>, bool), Box<dyn std::error::Error>> {
+    let (token_id, token_secret) = match bootstraptokenutil::parse_token(&token) {
+        Ok((id, secret)) => (id, secret),
+        Err(err) => {
+            // Token 格式不正确，忽略它。
+            return Ok((None, false));
+        }
+    };
+
+    let secret_name = format!("{}{}", bootstrapapi::BOOTSTRAP_TOKEN_SECRET_PREFIX, token_id);
+    let secret = match self.lister.get(&secret_name) {
+        Ok(s) => s,
+        Err(err) => {
+            if errors::is_not_found(&err) {
+                klog::v(3).infof("没有名为 %s 的 secret 来匹配 bootstrap bearer token", &secret_name);
+                return Ok((None, false));
+            }
+            return Err(err);
+        }
+    };
+
+    if secret.deletion_timestamp.is_some() {
+        token_errorf(secret, "已被删除并等待移除");
+        return Ok((None, false));
+    }
+
+    if secret.r#type.as_ref().map(|s| s.as_str()) != Some(bootstrapapi::SECRET_TYPE_BOOTSTRAP_TOKEN) || secret.data.is_none() {
+        token_errorf(secret, "类型无效，期望 %s。", bootstrapapi::SECRET_TYPE_BOOTSTRAP_TOKEN);
+        return Ok((None, false));
+    }
+
+    let ts = bootstrapsecretutil::get_data(secret, bootstrapapi::BOOTSTRAP_TOKEN_SECRET_KEY);
+    if subtle::constant_time_compare(ts.as_bytes(), token_secret.as_bytes()) != 1 {
+        token_errorf(secret, "键 %s 的值无效。", bootstrapapi::BOOTSTRAP_TOKEN_SECRET_KEY);
+        return Ok((None, false));
+    }
+
+    let id = bootstrapsecretutil::get_data(secret, bootstrapapi::BOOTSTRAP_TOKEN_ID_KEY);
+    if id != token_id {
+        token_errorf(secret, "键 %s 的值无效。", bootstrapapi::BOOTSTRAP_TOKEN_ID_KEY);
+        return Ok((None, false));
+    }
+
+    if bootstrapsecretutil::has_expired(secret, time::Now::now()) {
+        // 日志记录在 is_secret_expired 方法中完成。
+        return Ok((None, false));
+    }
+
+    if bootstrapsecretutil::get_data(secret, bootstrapapi::BOOTSTRAP_TOKEN_USAGE_AUTHENTICATION) != "true" {
+        token_errorf(secret, "未标记 %s=true。", bootstrapapi::BOOTSTRAP_TOKEN_USAGE_AUTHENTICATION);
+        return Ok((None, false));
+    }
+
+    let groups = match bootstrapsecretutil::get_groups(secret) {
+        Ok(g) => g,
+        Err(err) => {
+            token_errorf(secret, "键 %s 的值无效：%v。", bootstrapapi::BOOTSTRAP_TOKEN_EXTRA_GROUPS_KEY, err);
+            return Ok((None, false));
+        }
+    };
+
+    Ok((Some(authenticator::Response {
+        user: user::DefaultInfo {
+            name: format!("{}{}", bootstrapapi::BOOTSTRAP_USER_PREFIX, id),
+            groups: groups,
+        },
+    }), true))
 }
