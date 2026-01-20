d52d886134430b35b722085a37be2795f33506cc
wyjscg <1455573939@qq.com>
2026-01-20 19:09:19 +0800

    Update read.rs

--- read.rs
+++ read.rs
@@ -1,63 +1,66 @@
-struct GraphPopulator {
-    graph: Graph,
+struct NamedVertex {
+    name: String,
+    namespace: String,
+    id: i64,
+    vertex_type: VertexType,
 }
 
-pub fn add_graph_event_handlers(
-    graph: Graph,
-    nodes: NodeInformer,
-    pods: PodInformer,
-    pvs: PersistentVolumeInformer,
-    attachments: VolumeAttachmentInformer,
-    slices: Option<ResourceSliceInformer>,
-    pcrs: Option<PodCertificateRequestInformer>,
-) {
-    let g = GraphPopulator {
-        graph,
-    };
-
-    let pod_handler = pods.informer().add_event_handler(ResourceEventHandlerFuncs {
-        add_func: Some(g.add_pod),
-        update_func: Some(g.update_pod),
-        delete_func: Some(g.delete_pod),
-    });
-
-    let pvs_handler = pvs.informer().add_event_handler(ResourceEventHandlerFuncs {
-        add_func: Some(g.add_pv),
-        update_func: Some(g.update_pv),
-        delete_func: Some(g.delete_pv),
-    });
-
-    let attach_handler = attachments.informer().add_event_handler(ResourceEventHandlerFuncs {
-        add_func: Some(g.add_volume_attachment),
-        update_func: Some(g.update_volume_attachment),
-        delete_func: Some(g.delete_volume_attachment),
-    });
-
-    let mut synced = vec![
-        pod_handler.has_synced,
-        pvs_handler.has_synced,
-        attach_handler.has_synced,
-    ];
-
-    if let Some(slices) = slices {
-        let slice_handler = slices.informer().add_event_handler(ResourceEventHandlerFuncs {
-            add_func: Some(g.add_resource_slice),
-            update_func: None,
-            delete_func: Some(g.delete_resource_slice),
-        });
-        synced.push(slice_handler.has_synced);
+impl NamedVertex {
+    fn new(vertex_type: VertexType, namespace: String, name: String, id: i64) -> Self {
+        NamedVertex {
+            vertex_type,
+            name,
+            namespace,
+            id,
+        }
     }
 
-    if let Some(pcrs) = pcrs {
-        let pcr_handler = pcrs.informer().add_event_handler(ResourceEventHandlerFuncs {
-            add_func: Some(g.add_pcr),
-            update_func: None,
-            delete_func: Some(g.delete_pcr),
-        });
-        synced.push(pcr_handler.has_synced);
+    fn id(&self) -> i64 {
+        self.id
     }
 
-    std::thread::spawn(move || {
-        wait_for_named_cache_sync("node_authorizer", NeverStop, synced);
-    });
+    fn to_string(&self) -> String {
+        if self.namespace.is_empty() {
+            format!("{}:{}", vertex_types_str(self.vertex_type), self.name)
+        } else {
+            format!(
+                "{}:{}/{}",
+                vertex_types_str(self.vertex_type),
+                self.namespace,
+                self.name
+            )
+        }
+    }
+}
+
+struct DestinationEdge {
+    f: Box<dyn Node>,
+    t: Box<dyn Node>,
+    destination: Box<dyn Node>,
+}
+
+impl DestinationEdge {
+    fn new(from: Box<dyn Node>, to: Box<dyn Node>, destination: Box<dyn Node>) -> Self {
+        DestinationEdge {
+            f: from,
+            t: to,
+            destination,
+        }
+    }
+
+    fn from(&self) -> &dyn Node {
+        &*self.f
+    }
+
+    fn to(&self) -> &dyn Node {
+        &*self.t
+    }
+
+    fn weight(&self) -> f64 {
+        0.0
+    }
+
+    fn destination_id(&self) -> i64 {
+        self.destination.id()
+    }
 }
