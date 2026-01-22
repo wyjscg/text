127be693147bb4e122943b064bd78fce026aae0d
wyjscg <1455573939@qq.com>
2026-01-22 11:35:10 +0800

    Update read.rs

--- read.rs
+++ read.rs
@@ -1,168 +1,180 @@
+use std::collections::HashMap;
+
 impl Graph {
-    /// 删除从 fromType 到指定目标顶点的所有边
     fn delete_edges_locked(
-        &self,
+        &mut self,
         from_type: VertexType,
         to_type: VertexType,
-        to_namespace: String,
-        to_name: String,
+        to_namespace: &str,
+        to_name: &str,
     ) {
-        let mut data = self.lock.write().unwrap();
-        
-        // 获取目标顶点
-        let to_vert = match Self::get_vertex_from_data(
-            &data,
-            to_type,
-            &to_namespace,
-            &to_name,
-        ) {
+        let to_vert = match self.get_vertex_r_locked(to_type, to_namespace, to_name) {
             Some(v) => v,
             None => return,
         };
-        
-        // 收集需要删除的邻居和边
+
         let mut neighbors_to_remove = Vec::new();
         let mut edges_to_remove = Vec::new();
-        
-        // 访问所有指向 to_vert 的节点
-        let to_id = to_vert.id();
-        data.graph.visit_to(to_id, |from_node| {
-            // 将 from_node 转换为 NamedVertex
-            if let Some(from_vert) = data.graph.node(from_node.id()) {
-                if let Some(from_named) = from_vert.as_any().downcast_ref::<NamedVertex>() {
-                    // 只处理指定类型的顶点
-                    if from_named.vertex_type != from_type {
-                        return true;
-                    }
-                    
-                    // 如果这个邻居只有一条边（必定是指向我们的），也删除它
-                    if data.graph.degree(from_node.id()) == 1 {
-                        neighbors_to_remove.push(Arc::clone(from_vert));
-                    } else {
-                        // 否则只删除边
-                        if let Some(edge) = data.graph.edge_between(from_node.id(), to_id) {
-                            edges_to_remove.push(edge);
-                        }
-                    }
-                }
+
+        self.graph.visit_to(to_vert, |from| {
+            let from_vert = from.as_named_vertex();
+            if from_vert.vertex_type != from_type {
+                return true;
+            }
+
+            if self.graph.degree(from_vert) == 1 {
+                neighbors_to_remove.push(from_vert.clone());
+            } else {
+                edges_to_remove.push(self.graph.edge_between(from, to_vert));
             }
             true
         });
-        
-        // 清理孤立的顶点
+
         for v in neighbors_to_remove {
-            Self::remove_vertex_from_data(&mut data, &v);
+            self.remove_vertex_locked(&v);
         }
-        
-        // 删除边并更新目标索引
+
         for edge in edges_to_remove {
-            data.graph.remove_edge(edge.from(), edge.to());
-            Self::remove_edge_from_destination_index_locked(&mut data, &edge);
+            self.graph.remove_edge(&edge);
+            self.remove_edge_from_destination_index_locked(&edge);
         }
     }
-    
-    /// 从目标索引中移除边的快速路径
-    fn remove_edge_from_destination_index_locked(
-        data: &mut GraphData,
-        edge: &Arc<dyn Edge>,
-    ) {
-        let from_id = edge.from();
-        
-        // 对于边数较少的节点不维护索引
-        let edge_count = data.graph.degree(from_id);
-        if edge_count < data.destination_edge_threshold {
-            data.destination_edge_index.remove(&from_id);
+
+    fn remove_edge_from_destination_index_locked(&mut self, e: &dyn Edge) {
+        let n = e.from();
+        let edge_count = self.graph.degree(n);
+
+        if edge_count < self.destination_edge_threshold {
+            self.destination_edge_index.remove(&n.id());
             return;
         }
-        
-        // 如果索引存在，减少 nodeID->destinationID 的引用计数
-        if let Some(index) = data.destination_edge_index.get_mut(&from_id) {
-            if let Some(dest_edge) = edge.as_any().downcast_ref::<DestinationEdge>() {
-                index.decrement(dest_edge.destination_id());
+
+        if let Some(index) = self.destination_edge_index.get_mut(&n.id()) {
+            if let Some(destination_edge) = (e as &dyn std::any::Any).downcast_ref::<DestinationEdge>() {
+                index.decrement(destination_edge.destination_id());
             }
         }
     }
-    
-    /// 向目标索引添加边的快速路径
-    fn add_edge_to_destination_index_locked(
-        data: &mut GraphData,
-        edge: &Arc<dyn Edge>,
-    ) {
-        let from_id = edge.from();
-        
-        // 检查是否已有索引
-        if !data.destination_edge_index.contains_key(&from_id) {
-            // 没有索引，使用完整的索引计算方法
-            Self::recompute_destination_index_locked(data, from_id);
+
+    fn add_edge_to_destination_index_locked(&mut self, e: &dyn Edge) {
+        let n = e.from();
+        let n_id = n.id();
+
+        if !self.destination_edge_index.contains_key(&n_id) {
+            self.recompute_destination_index_locked(n);
             return;
         }
-        
-        // 快速添加新边到现有索引
-        if let Some(index) = data.destination_edge_index.get_mut(&from_id) {
-            if let Some(dest_edge) = edge.as_any().downcast_ref::<DestinationEdge>() {
-                index.increment(dest_edge.destination_id());
+
+        if let Some(index) = self.destination_edge_index.get_mut(&n_id) {
+            if let Some(destination_edge) = (e as &dyn std::any::Any).downcast_ref::<DestinationEdge>() {
+                index.increment(destination_edge.destination_id());
             }
         }
     }
-    
-    /// 必须在写锁下调用
-    /// 从图和维护的索引中删除指定的顶点
-    /// 不会影响邻居顶点的索引
-    fn remove_vertex_locked(&self, v: &Arc<dyn Node>) {
-        let mut data = self.lock.write().unwrap();
-        Self::remove_vertex_from_data(&mut data, v);
-    }
-    
-    /// 内部辅助方法：从 GraphData 中删除顶点
-    fn remove_vertex_from_data(data: &mut GraphData, v: &Arc<dyn Node>) {
-        let node_id = v.id();
-        
-        // 从图中删除节点
-        data.graph.remove_node(node_id);
-        
-        // 从目标边索引中删除
-        data.destination_edge_index.remove(&node_id);
-        
-        // 从 vertices map 中删除
-        if let Some(named_vertex) = v.as_any().downcast_ref::<NamedVertex>() {
-            if let Some(namespace_map) = data.vertices.get_mut(&named_vertex.vertex_type) {
-                if let Some(name_map) = namespace_map.get_mut(&named_vertex.namespace) {
-                    name_map.remove(&named_vertex.name);
-                    
-                    // 如果 namespace 下没有节点了，删除整个 namespace
-                    if name_map.is_empty() {
-                        namespace_map.remove(&named_vertex.namespace);
-                    }
+
+    fn remove_vertex_locked(&mut self, v: &NamedVertex) {
+        self.graph.remove_node(v);
+        self.destination_edge_index.remove(&v.id());
+
+        if let Some(namespace_map) = self.vertices.get_mut(&v.vertex_type) {
+            if let Some(name_map) = namespace_map.get_mut(&v.namespace) {
+                name_map.remove(&v.name);
+                if name_map.is_empty() {
+                    namespace_map.remove(&v.namespace);
                 }
             }
         }
     }
-    
-    /// 重新计算节点的目标索引
-    fn recompute_destination_index_locked(data: &mut GraphData, node_id: i32) {
-        // 对于边数较少的节点不维护索引
-        let edge_count = data.graph.degree(node_id);
-        if edge_count < data.destination_edge_threshold {
-            data.destination_edge_index.remove(&node_id);
+
+    fn recompute_destination_index_locked(&mut self, n: &dyn Node) {
+        let edge_count = self.graph.degree(n);
+        let n_id = n.id();
+
+        if edge_count < self.destination_edge_threshold {
+            self.destination_edge_index.remove(&n_id);
             return;
         }
+
+        let index = self.destination_edge_index
+            .entry(n_id)
+            .or_insert_with(|| IntSet::new());
         
-        // 获取或创建索引
-        let index = data.destination_edge_index
-            .entry(node_id)
-            .or_insert_with(IntSet::new);
-        
-        // 重置索引
-        index.reset();
-        
-        // 填充索引
-        data.graph.visit_from(node_id, |dest_node| {
-            if let Some(edge) = data.graph.edge_between(node_id, dest_node.id()) {
-                if let Some(dest_edge) = edge.as_any().downcast_ref::<DestinationEdge>() {
-                    index.increment(dest_edge.destination_id());
+        if self.destination_edge_index.contains_key(&n_id) {
+            index.reset();
+        }
+
+        self.graph.visit_from(n, |dest| {
+            if let Some(edge) = self.graph.edge_between(n, dest) {
+                if let Some(destination_edge) = (edge as &dyn std::any::Any).downcast_ref::<DestinationEdge>() {
+                    index.increment(destination_edge.destination_id());
                 }
             }
             true
         });
     }
+}
+
+struct DestinationEdge {
+    f: Box<dyn Node>,
+    t: Box<dyn Node>,
+    destination: Box<dyn Node>,
+}
+
+impl DestinationEdge {
+    fn destination_id(&self) -> i32 {
+        self.destination.id()
+    }
+}
+
+impl Edge for DestinationEdge {
+    fn from(&self) -> &dyn Node {
+        self.f.as_ref()
+    }
+
+    fn to(&self) -> &dyn Node {
+        self.t.as_ref()
+    }
+
+    fn weight(&self) -> f64 {
+        0.0
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct NamedVertex {
+    name: String,
+    namespace: String,
+    id: i32,
+    vertex_type: VertexType,
+}
+
+impl Node for NamedVertex {
+    fn id(&self) -> i32 {
+        self.id
+    }
+}
+
+struct Graph {
+    graph: DirectedAcyclicGraph,
+    vertices: HashMap<VertexType, NamespaceVertexMapping>,
+    destination_edge_index: HashMap<i32, IntSet>,
+    destination_edge_threshold: usize,
+}
+
+trait Node {
+    fn id(&self) -> i32;
+}
+
+trait Edge {
+    fn from(&self) -> &dyn Node;
+    fn to(&self) -> &dyn Node;
+    fn weight(&self) -> f64;
+}
+
+trait GraphTrait {
+    fn has(&self, node: &dyn Node) -> bool;
+    fn nodes(&self) -> Vec<&dyn Node>;
+    fn from(&self, node: &dyn Node) -> Vec<&dyn Node>;
+    fn has_edge_between(&self, x: &dyn Node, y: &dyn Node) -> bool;
+    fn edge(&self, u: &dyn Node, v: &dyn Node) -> Option<&dyn Edge>;
 }
